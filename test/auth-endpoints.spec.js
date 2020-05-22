528bf67e06fefa4bbf70e0f54f9afd632b9df5e4
Olivia Davis <olivia.davis593@gmail.com>
2020-05-21 20:42:46 -0500

    JWT tests failing

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -3,7 +3,7 @@ const app = require('../src/app')
 const jwt = require('jsonwebtoken')
 const helpers = require('./items.fixtures')
 
-describe.only('Auth Endpoints', function() {
+describe('Auth Endpoints', function() {
   let db
 
   const { testUsers } = helpers.makeItemsFixtures()
