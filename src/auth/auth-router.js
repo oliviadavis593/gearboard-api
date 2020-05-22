98b59f5a7abef0c9e193c956eddc45f6fb1738b7
Olivia Davis <olivia.davis593@gmail.com>
2020-05-21 19:40:56 -0500

    send JWT in response

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -9,29 +9,39 @@ authRouter
        const { email, password } = req.body
        const loginUser = { email, password }
        console.log("loginUser", loginUser)
+
        for (const [key, value] of Object.entries(loginUser))
             if(value == null)
                 return res.status(400).json({
                     error: `Missing '${key}' in request body`
                 })
+
+
         AuthService.getUserWithEmail(
             req.app.get('db'),
             loginUser.email
         )
+
             .then(dbUser => {
                 console.log("dbUser", dbUser)
                 if (!dbUser)
                     return res.status(400).json({
                         error: 'Incorrect email or password'
                     })
-                    return AuthService.comparePasswords(loginUser.password, dbUser.password)
-                        .then(compareMatch => {
-                            console.log("compareMatch", compareMatch)
-                            if (!compareMatch)
-                                return res.status(400).json({
-                                    error: 'Incorrect email or password'
-                                })
-                                res.send('ok')
+
+
+            return AuthService.comparePasswords(loginUser.password, dbUser.password)
+                .then(compareMatch => {
+                    console.log("compareMatch", compareMatch)
+                        if (!compareMatch)
+                            return res.status(400).json({
+                                error: 'Incorrect email or password'
+                            })
+                            const sub = dbUser.email
+                            const payload = { email: dbUser.email }
+                            res.send({
+                                authToken: AuthService.createJwt(sub, payload),
+                            })
                         })
             })
             .catch(next)
